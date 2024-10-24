857e0a372bda42ae5655f2f7c5518342e0eb5f14
Tacha <Anastasiykoshe@gmail.com>
2024-10-24 06:56:37 +0300

    Модуль 13 задание 2 Дополнено для задания 3

--- hw2.py
+++ hw2.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
+
 api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -9,12 +10,12 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
 async def all_messages(message):
-    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
