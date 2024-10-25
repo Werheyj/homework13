f88ac9c0213daadd83a4be3cc83bd4c6ae961b97
Tacha <Anastasiykoshe@gmail.com>
2024-10-25 15:48:02 +0300

    Модуль 13 задание 2 Дополнено для задания 3 Дополнено для задания 4 Дополнено для задания 5 Дополнено для задания 6

--- hw2.py
+++ hw2.py
@@ -2,10 +2,11 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
 
-api = ''
+api = '7640886664:AAEtfUb7RnfekSFH0B5JjwjMu2ZGKT9aNNY'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -22,6 +23,14 @@ button2 = KeyboardButton(text='Информация')
 kb.add(button1)
 kb.add(button2)
 
+kbI = InlineKeyboardMarkup()
+buttonInline1 = InlineKeyboardButton(text='Рассчитать норму калорий',
+                                     callback_data='calories')
+buttonInline2 = InlineKeyboardButton(text='Формулы расчёта',
+                                     callback_data='formulas')
+kbI.add(buttonInline1)
+kbI.add(buttonInline2)
+
 
 @dp.message_handler(commands=['start'])
 async def start(message):
@@ -30,9 +39,23 @@ async def start(message):
 
 
 @dp.message_handler(text='Рассчитать')
-async def set_age(message):
-    await message.answer('Введите свой возраст:')
+async def main_menu(message):
+    await message.answer('Выберите опцию:',
+                         reply_markup=kbI)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('Формула Миффлина-Сан Жеора: '
+                      '10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161')
+    await call.answer()
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
     await UserState.age.set()
+    # await call.answer()
 
 
 @dp.message_handler(state=UserState.age)
